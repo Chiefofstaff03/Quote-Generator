36dec1f3c47f3da6eb8d425b2060765b91d2333f
nikhil <nikhil.connectnow@gmail.com>
2025-06-16 04:09:37 +0530

    code splitting and img functionality

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const connectDB = require('./config/db'); 
+const connectDB = require('./config/db'); // db.js ko sahi jagah shift karke import
 const helmet = require('helmet');
 const compression = require('compression');
 
